081107d9f6edcb768c5e73382d4a289f892e4ea5
PrinceSinghdoya <232084597+PrinceSinghdoya@users.noreply.github.com>
2025-12-18 22:29:25 +0530

    Add files via upload
    A simple browser-based quiz application built with JavaScript that uses prompts and alerts to ask questions, validate user input, and calculate the final score. Designed to practice core JavaScript concepts like arrays, loops, and conditional logic.

---
+++ LAB 04/quiz.js
@@ -0,0 +1,32 @@
+
+var questions = [
+    "What is the capital of India?",
+    "How many days are in a week?",
+    "Which language runs in the browser? (js, python, c++)",
+    "What is 5 + 5?",
+    "Which is used to style web pages? (css/html/js)"
+];
+
+var answers = [
+    "delhi",
+    "7",
+    "js",
+    "10",
+    "css"
+];
+var score = 0;
+for (var i = 0; i < questions.length; i++) { 
+    var userAnswer = prompt(questions[i]);
+    if (userAnswer === null) {
+        alert("Quiz stopped.");
+        break;
+    }
+    userAnswer = userAnswer.toLowerCase().trim();
+    if (userAnswer === answers[i]) {
+        alert("Correct!");
+        score++;
+    } else {
+        alert("Wrong! Correct answer is: " + answers[i]);
+    }
+}
+alert("Your final score: " + score + " / " + questions.length);
